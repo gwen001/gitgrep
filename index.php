354b726e02011ff9fbdf224c0154294189b01776
Gwen <g@10degres.net>
2020-06-26 13:43:27 +0200

    github sponsor

--- index.php
+++ index.php
@@ -61,8 +61,7 @@ if( !isset($_GET['r']) || ($r=trim($_GET['r'])) == '' ) {
                     This webservice is my contribution to the security industry, if you like it, you can support my work.
                 </p>
                 <p class="text-center">
-                    <a href="https://www.buymeacoffee.com/gwendallecoguic" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-yellow.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" width="217" ></a>
-                </p>
+                    <iframe src="https://github.com/sponsors/gwen001/button" title="Sponsor gwen001" height="35" width="107" style="border: 0;"></iframe>
                 <p>
                     Found a bug? Feel free to open an issue on <a href="https://github.com/gwen001/gitgrep" target="_blank">the official repository</a>.
                 </p>
